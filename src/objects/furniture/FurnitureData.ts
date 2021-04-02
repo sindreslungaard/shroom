804cccbf05ea1e7bab195be6f091c630ccea6b94
Sindre Slungaard <slungaards@gmail.com>
2021-04-02 07:12:46 +0200

    add placementtype to furniinfo

--- src/objects/furniture/FurnitureData.ts
+++ src/objects/furniture/FurnitureData.ts
@@ -94,6 +94,9 @@ export class FurnitureData implements IFurnitureData {
 
         typeToInfo[type] = formatFurnitureData(element);
 
+        // @ts-ignore
+        typeToInfo[type].placementType = furnitureType
+
         if (furnitureType === "floor") {
           if (floorIdToType[id] != null)
             throw new Error(`Floor furniture with id ${id} already exists`);
@@ -108,8 +111,8 @@ export class FurnitureData implements IFurnitureData {
       });
     };
 
-    register(parsed.furnidata.roomitemtypes[0].furnitype, "wall");
-    register(parsed.furnidata.wallitemtypes[0].furnitype, "floor");
+    register(parsed.furnidata.roomitemtypes[0].furnitype, "floor");
+    register(parsed.furnidata.wallitemtypes[0].furnitype, "wall");
 
     return { typeToInfo, floorIdToType, wallIdToType };
   }
